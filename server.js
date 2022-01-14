efc93c5f28acf9a25f485cc7dd4587a8f41988ca
sandymist <95550935+sandymist@users.noreply.github.com>
2022-01-13 19:27:12 -0800

    Show headers

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
 const http = require('http')
 
-const port = process.env.PORT || 7911
+const port = process.env.PORT || 8000
 
-const server = http.createServer((req, res) => {
+const router = (req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    const fruits = [
-        "apples",
-        "passion",
-        "kiwi",
-        "strawberries"
-    ]
-    const body = fruits.map(function(fruit) {
-        return {name: fruit}
-    })
-    res.end(JSON.stringify(body))
-})
+
+    console.log("PATH: " + req.url)
+    console.log("REQ HEADERS: " + JSON.stringify(req.headers))
+    var body = ""
+    switch (req.url) {
+    case "/":
+        const fruits = [
+            "apples",
+            "passion",
+            "kiwi",
+            "strawberries"
+        ]
+        body = fruits.map(function(fruit) {
+            return {name: fruit}
+        })
+        break
+    case "/names":
+        const names = [
+            "Bob",
+            "John",
+            "Steve",
+        ]
+        body = names.map(function(name) {
+            return {name: name}
+        })
+        break
+    }
+    res.end(JSON.stringify({status: "Ok", data: body}))
+    console.log("Sent response, response headers: ", res.getHeaders())
+}
+
+const server = http.createServer(router)
 
 server.listen(port, () => {
     console.log(`Server running at port ${port}`)
